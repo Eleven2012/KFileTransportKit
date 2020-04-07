09dfb6ade4070c146e756ad891f883161f0cc690
yulu.kong <yulu.kong@ubtrobot.com>
2020-04-07 15:04:36 +0800

    init project

---
+++ ios/swift/KFileTransportKit/KFileTransportKit/KFileTransportKit.h
@@ -0,0 +1,19 @@
+//
+//  KFileTransportKit.h
+//  KFileTransportKit
+//
+//  Created by yulu kong on 2020/4/7.
+//  Copyright © 2020 yulu kong. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for KFileTransportKit.
+FOUNDATION_EXPORT double KFileTransportKitVersionNumber;
+
+//! Project version string for KFileTransportKit.
+FOUNDATION_EXPORT const unsigned char KFileTransportKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KFileTransportKit/PublicHeader.h>
+
+
